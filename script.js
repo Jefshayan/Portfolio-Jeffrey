0ff54baedced3b740eb311a82191c69f091ea911
Jeffrey <eschylle.jeffrey@gmail.com>
2018-05-08 15:11:53 -0400

    responsive design

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ $(document).ready(function(){
 	    $('#slider1').parallax("center", 0, 0.1, true);
 	    $('#slider2').parallax("center", 900, 0.1, true);
 	    $('#slider3').parallax("center", 2900, 0.1, true);
-		$('#slider4').parallax("center", 900, 0.1, true);
+	    $('#slider4').parallax("center", 900, 0.1, true);
 		});
+
+/*
 $(document).ready(function(){
   var height = $(window).height();
   $("#slider1").height(height);
@@ -14,3 +16,14 @@ $(document).ready(function(){
   
 
 });
+$(document).ready(function(){
+  var height = $(window).height();
+  $("#slider2").height(height);
+  $(window).scroll(function(){
+     height = $(window).height();
+     $("#slider2").height(height);
+  });
+  
+
+});
+*/
